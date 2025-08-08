76c5af62fd6a9a6016afc7007d63671d5f911e82
srushtibhilare <srushti29bhilare@gmail.com>
2025-08-08 13:59:47 +0530

    Describe your changes here

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
